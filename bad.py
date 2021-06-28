7f03f9a8988d45e46c0b03e631ed5474084a5a48
Jeremy Paige <ucodery@gmail.com>
2021-06-28 15:01:30 -0700

    v0.0.1 commit

---
+++ bad.py
@@ -0,0 +1,6 @@
+"""please, Please, PLEASE, don't install"""
+
+__version__ = "0.0.1"
+
+if __name__ == '__main__':
+    """Nothing to see here"""
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    py_modules=["bad"],
+    name="bad_project",
+    version="0.0.1",
+    description="please, Please, PLEASE, don't install",
+    install_requires=["attrs==20.2.0", "black"],
+)
